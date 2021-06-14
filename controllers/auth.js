afdd87ca4c9d79e86a43c2fceff3413159455ff0
AlanDo <daidx97@gmail.com>
2021-06-14 23:52:55 +0700

    add user hashed_password method

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
-exports.signup= (req,res)=>{
-    const{ name, email, password}=req.body
-    res.json({
-        user:{name, email,password}
-    })
-}
+const User = require("../models/user");
+const shortId = require("shortid");
+exports.signup = (req, res) => {
+  const { name, email, password } = req.body;
+  res.json({
+    user: { name, email, password },
+  });
+};
--- models/user.js
+++ models/user.js
@@ -1,55 +1,90 @@
-const { Mongoose } = require("mongoose")
-const crypto = require('crypto')
-const { stringify } = require("querystring")
+const { Mongoose } = require("mongoose");
+const crypto = require("crypto");
+const { stringify } = require("querystring");
 
-const userSchema = new Mongoose.Schema({
+const userSchema = new Mongoose.Schema(
+  {
     username: {
-        type: String,
-        trim: true,
-        required: true,
-        maxLength: 32,
-        unique: true,
-        index: true,
-        lowerCase: true
+      type: String,
+      trim: true,
+      required: true,
+      maxLength: 32,
+      unique: true,
+      index: true,
+      lowerCase: true,
     },
     name: {
-        type: String,
-        trim: true,
-        required: true,
-        maxLength: 32
+      type: String,
+      trim: true,
+      required: true,
+      maxLength: 32,
     },
     email: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true,
-        maxLength: 32,
-        lowerCase: true
-
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      maxLength: 32,
+      lowerCase: true,
     },
     profile: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     hashed_password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     salt: String,
     about: {
-        type: String
+      type: String,
     },
     role: {
-        type: Number,
-        trim: true
+      type: Number,
+      trim: true,
     },
     photo: {
-        data: Buffer,
-        contentType: String
+      data: Buffer,
+      contentType: String,
     },
     resetPasswordLink: {
-        data: String,
-        default: ''
+      data: String,
+      default: "",
+    },
+  },
+  { timestamp: true }
+);
+
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    //create a temporarity variable called _password
+    this._password = password;
+    //generate salt
+    this.salt = this.makeSalt();
+    //encryptPassword
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+userSchema.method = {
+    authenticate: function(plantText){
+        return this.encryptPassword(plantText)= this.hashed_password
+    },
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
     }
-}, { timestamp: true })
-module.exports = mongoose.model('User', userSchema)
+  },
+  makeSalt: function(){
+      return Math.round(new Date().valueOf() * Math.random() + '');
+  }
+};
+module.exports = mongoose.model("User", userSchema);
